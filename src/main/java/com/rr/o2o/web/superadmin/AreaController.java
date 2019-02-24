5a0a638e5cb26f3cdd21c5425475598b98a7db73
rr <thinkerr24@163.com>
2019-02-24 23:36:59 +0800

    添加logback日志组件并再AreaController中测试

--- src/main/java/com/rr/o2o/web/superadmin/AreaController.java
+++ src/main/java/com/rr/o2o/web/superadmin/AreaController.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +22,14 @@ import com.rr.o2o.service.impl.AreaService;
 public class AreaController {
 	
 
+	private Logger logger = LoggerFactory.getLogger(AreaController.class);
 	@Autowired
 	private AreaService areaService;
 	@RequestMapping(value="/listarea", method=RequestMethod.GET)
 	@ResponseBody
 	private Map<String, Object> listArea() {
+		logger.info("===start===");
+		long startTime = System.currentTimeMillis();
 		Map<String, Object> modelMap = new HashMap<>();
 		List<Area> list = new ArrayList<>();
 		try {
@@ -35,6 +41,10 @@ public class AreaController {
 			modelMap.put("success", false);
 			modelMap.put("errMsg", e.toString());
 		}
+		logger.error("test error!");
+		long endTime = System.currentTimeMillis();
+		logger.debug("costTime:[{}ms]", endTime - startTime);
+		logger.info("===end===");
 		return modelMap;
 	}
 }
